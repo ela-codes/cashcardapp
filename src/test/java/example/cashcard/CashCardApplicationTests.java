eab943bd8c701802f0c72f5cafd42377e21b397f
Ela <103253928+ela-codes@users.noreply.github.com>
2024-07-30 15:58:12 -0500

    add test for GET cash card

--- src/test/java/example/cashcard/CashCardApplicationTests.java
+++ src/test/java/example/cashcard/CashCardApplicationTests.java
@@ -1,13 +1,31 @@
 package example.cashcard;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+// starts the spring boot application so our tests can perform requests to it
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CashCardApplicationTests {
+    @Autowired
+    // inject "TestRestTemplate" test helper that’ll allow us to make HTTP requests to the locally running application
+    TestRestTemplate restTemplate;
 
 	@Test
-	void contextLoads() {
+	void shouldReturnACashCardWhenDataIsSaved() {
+        // this is a GET request to our application endpoint /cashcards/99
+        // restTemplate returns a ResponseEntity which provides info about what happened to the request
+        ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/99", String.class);
+        //<String> specifies that the ResponseEntity instance will contain a response body of type String.
+
+        // ResponseEntity in the Spring Framework used to represent an HTTP response, including the status code, headers, and body.
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        
 	}
 
 }
--- src/test/java/example/cashcard/CashCardJsonTests.java
+++ src/test/java/example/cashcard/CashCardJsonTests.java
@@ -39,4 +39,17 @@ class CashCardJsonTest {
         assertThat(json.write(cashCard)).hasJsonPathNumberValue("@.amount");
         assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@.amount").isEqualTo(123.45);
     }
+
+    @Test
+    void cashCardDeserializationTest() throws IOException {
+        String expected = """
+                {
+                    "id": 99,
+                    "amount": 123.45
+                }
+                """;
+        assertThat(json.parse(expected)).isEqualTo(new CashCard(99L, 123.45));
+        assertThat(json.parseObject(expected).id()).isEqualTo(99);
+        assertThat(json.parseObject(expected).amount()).isEqualTo(123.45);
+    }
 } 
