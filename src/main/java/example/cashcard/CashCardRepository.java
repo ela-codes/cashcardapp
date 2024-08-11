b6991f252ae820f5e2975d06900f0a08a992794a
Ela <103253928+ela-codes@users.noreply.github.com>
2024-08-11 18:23:43 -0500

    Implement DELETE method and test

--- src/main/java/example/cashcard/CashCardRepository.java
+++ src/main/java/example/cashcard/CashCardRepository.java
@@ -6,7 +6,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 
 interface CashCardRepository extends CrudRepository<CashCard, Long>, PagingAndSortingRepository<CashCard, Long> {
+    // the method name follows a naming convention that Spring Data JDBC recognizes
+    // it parses the name in order to create an SQL query
+    
     CashCard findByIdAndOwner(Long id, String owner);
     Page<CashCard> findByOwner(String owner, PageRequest pageRequest);
+    boolean existsByIdAndOwner(Long id, String owner);
 
 }
--- src/main/java/example/cashcard/SecurityConfig.java
+++ src/main/java/example/cashcard/SecurityConfig.java
@@ -41,6 +41,12 @@ class SecurityConfig {
             .password(passwordEncoder.encode("qrs456"))
             .roles("NON-OWNER") // new role
             .build();
+
+        UserDetails kumar = users
+            .username("kumar2")
+            .password(passwordEncoder.encode("xyz789"))
+            .roles("CARD-OWNER")
+            .build();
         return new InMemoryUserDetailsManager(sarah, hankOwnsNoCards);
 
     }
